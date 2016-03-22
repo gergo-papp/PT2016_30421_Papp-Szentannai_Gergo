542aab1fde36d4a5d72f7e6f7ad7bd2127890352
Papp Szentannai Gergő <gergo_13@yahoo.com>
2016-03-22 12:47:16 +0200

    Created OrderManagement project
    [empty project]

---
+++ orderManagement/src/main/java/utcn/pt/orderManagement/App.java
@@ -0,0 +1,14 @@
+package utcn.pt.orderManagement;
+
+/**
+ * Entry point of the program.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //TODO: Create thread and open MainFrame
+    	
+    }
+}
